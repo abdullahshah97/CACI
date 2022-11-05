1028fc3404b03ff048f0cfca49a222d89bb130fc
Abdullah Shah <abdullah-shah97@hotmail.com>
2022-11-05 22:18:06 +0000

    Initial commit

---
+++ src/main/java/com/cacibricks/demo/CaciBricksApplication.java
@@ -0,0 +1,13 @@
+package com.cacibricks.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaciBricksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaciBricksApplication.class, args);
+	}
+
+}
